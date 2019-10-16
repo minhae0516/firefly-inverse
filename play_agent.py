e9df58085e0ec206d881ebbb675decefb4de5605
Minhae Kwon <33565760+minhae0516@users.noreply.github.com>
2019-10-16 11:24:31 -0500

    agent file name

--- play_agent.py
+++ play_agent.py
@@ -30,12 +30,12 @@ torch.backends.cudnn.benchmark = False
 import datetime
 import pandas as pd
 
-filename = '20191015-161114-1' # agent information
+filename = '20191015-161114' # agent information
 df = pd.read_csv('../firefly-inverse-data/data/' + filename + '_log.csv',
                  usecols=['discount_factor','process gain forward', 'process gain angular', 'process noise std forward',
                           'process noise std angular', 'obs gain forward', 'obs gain angular', 'obs noise std forward',
                           'obs noise std angular', 'goal radius'])
-                          
+
 DISCOUNT_FACTOR = df['discount_factor'][0]
 gains_range = [np.floor(df['process gain forward'].min()), np.ceil(df['process gain forward'].max()),
                np.floor(df['process gain angular'].min()), np.ceil(df['process gain angular'].max())]
@@ -52,7 +52,7 @@ x, b, state, pro_gains, pro_noise_stds, obs_gains, obs_noise_stds, goal_radius  
 state_dim = env.state_dim
 action_dim = env.action_dim
 
-MAX_EPISODE = 100
+MAX_EPISODE = 10
 std = 0.00001 #0.05
 noise = Noise(action_dim, mean=0., std=std)
 
